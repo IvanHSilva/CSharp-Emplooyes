c3862e3f6b3bf2eeb051b251e640826dc1aa7eb8
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-08-16 21:38:38 -0300

    Add project files.

---
+++ Emplooyes/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Emplooyes {
+    public class Employee {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void increaseSalaray(double percent) {
+            Salary += Salary * (percent / 100);
+        }
+
+        public override string ToString() {
+            return $"{Id}, {Name}, {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Emplooyes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Emplooyes {
+    internal class Program {
+        static void Main(string[] args) {
+
+            List<Employee> employee = new List<Employee>();
+            Console.Write("Número de funcionários: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine($"Funcionário #{i + 1}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                employee.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.Write("Id do funcionário: ");
+            int empid = int.Parse(Console.ReadLine());
+            Employee emp = employee.Find(x => x.Id == empid);
+            if (emp != null) {
+                Console.Write("Porcentagem do aumento: ");
+                double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.increaseSalaray(percent);
+            }
+            else {
+                Console.WriteLine("Id do funcionário não encontrado");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Lista dos funcionários atualizada:");
+            foreach (Employee e in employee) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
